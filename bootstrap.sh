da2629c9b6bd8f71fe869f8be204917e03624419
PG1003 <34630287+PG1003@users.noreply.github.com>
2020-08-29 11:58:09 +0200

    Fixed compatibility for BSD like platforms
    'stat' is not compatible between GNU and BSD / macOS.
    Suggested by LHF.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,13 @@
 # EXE and OUTPUT can target the same file which in this case will
 # append SCRIPT to EXE
 
-PAYLOAD=$(printf "\r\n-- dogfood %08X\r\n" $(stat -L -c %s $1))
+PAYLOAD=$(printf "\r\n-- dogfood %08X\r\n" $(wc -c < $1))
 
 if [[ ! $1 -ef $3 ]]
 then
   cp -f $1 $3
 fi
 
-printf "\r\n-- food %08X\r\n" $(stat -L -c %s $2) >> $3
+printf "\r\n-- food %08X\r\n" $(wc -c < $2) >> $3
 cat $2 >> $3
 echo $PAYLOAD >> $3
